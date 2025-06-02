cb3be24e2960646d172d4e625bb7abab68789412
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-06-02 18:05:28 +0530

    Time: 0 ms (100%), Space: 42.4 MB (28.03%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,44 +1,61 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public void flatten(TreeNode root) {
-        if(root==null)
-        return;
-        List<Integer> list= new ArrayList<>();
-        preorder(root,list);
-        int len=list.size();
-        TreeNode Root=root;
-        for(int i=1;i<len;i++)
-        {
-            TreeNode node = new TreeNode(list.get(i));
-            Root.left=null;
-            Root.right=node;
-            Root=Root.right;
-        }
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if(root==null)
+//         return;
+//         List<Integer> list= new ArrayList<>();
+//         preorder(root,list);
+//         int len=list.size();
+//         TreeNode Root=root;
+//         for(int i=1;i<len;i++)
+//         {
+//             TreeNode node = new TreeNode(list.get(i));
+//             Root.left=null;
+//             Root.right=node;
+//             Root=Root.right;
+//         }
         
         
-    }
-    public void  preorder(TreeNode root,List<Integer> list)
-    {
-        if(root==null)
-        return;
-        list.add(root.val);
-        preorder(root.left,list);
-        preorder(root.right,list);
-    }
+//     }
+//     public void  preorder(TreeNode root,List<Integer> list)
+//     {
+//         if(root==null)
+//         return;
+//         list.add(root.val);
+//         preorder(root.left,list);
+//         preorder(root.right,list);
+//     }
 
+
+// }
+class Solution {
+    TreeNode prev=null;
+        public void flatten(TreeNode root) {
+            
+            if(root==null)
+            return ;
+             flatten(root.right);
+            flatten(root.left);
+           
+            root.right=prev;
+            root.left=null;
+            prev=root;
+ 
+ 
+     }
 
 }
