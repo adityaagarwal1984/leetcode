924bd670a796f582b87d1b70beb93123bbea9474
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-06-17 23:56:48 +0530

    Time: 0 ms (100%), Space: 42.1 MB (49.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero=0,one=0,two=0;
-        for(int i=0;i<nums.length;i++)
+        
+        //we will try to do this by dutch national algorithm
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+        if(nums[mid]==0)
         {
-            if(nums[i]==0)
-            zero++;
-            if(nums[i]==1)
-            one++;
-            if(nums[i]==2)
-            two++;
+            swap(nums,mid,low);
+            mid++;
+            low++;
         }
-        for(int i=0;i<zero;i++)
-        nums[i]=0;
-        for(int j=zero;j<one+zero;j++)
-        nums[j]=1;
-        for(int k=zero+one;k<one+zero+two;k++)
-        nums[k]=2;
+        else if(nums[mid]==1)
+        mid++;
+        else if(nums[mid]==2)
+        {
+            swap(nums,mid,high);
+            high--;
+        }
+    
+        }
         
         
+    }
+    public void swap(int arr[],int i,int j )
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
