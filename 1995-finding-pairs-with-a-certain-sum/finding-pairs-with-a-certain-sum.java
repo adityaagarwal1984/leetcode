ec59f8f04073d57f8f1ac346e3d8bef16334f157
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-07-06 22:02:49 +0530

    Time: 142 ms (76.14%) | Memory: 74 MB (75.79%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,35 @@
+class FindSumPairs {
+    int[] nums1;
+    int[] nums2;
+    Map<Integer, Integer> freqMap2;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        freqMap2 = new HashMap<>();
+        for (int num : nums2) {
+            freqMap2.put(num, freqMap2.getOrDefault(num, 0) + 1);
+        }
+    }
+
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        freqMap2.put(oldVal, freqMap2.get(oldVal) - 1);
+        if (freqMap2.get(oldVal) == 0) {
+            freqMap2.remove(oldVal);
+        }
+
+        nums2[index] += val;
+        int newVal = nums2[index];
+        freqMap2.put(newVal, freqMap2.getOrDefault(newVal, 0) + 1);
+    }
+
+    public int count(int tot) {
+        int count = 0;
+        for (int num1 : nums1) {
+            int complement = tot - num1;
+            count += freqMap2.getOrDefault(complement, 0);
+        }
+        return count;
+    }
+}
