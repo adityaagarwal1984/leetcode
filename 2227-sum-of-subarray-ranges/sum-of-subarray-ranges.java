4b2aa9c632b46a842f380f01317467099cdef99b
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-07-12 21:56:37 +0530

    Time: 26 ms (37.60%) | Memory: 45.5 MB (5.56%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,19 +1,86 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        long sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int min=nums[i];
-            int max=nums[i];
+    public long subArrayRanges(int[] arr) {
+        // long sum=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     int min=nums[i];
+        //     int max=nums[i];
 
-            for(int j=i;j<nums.length;j++)
-            {
-                max=Math.max(max,nums[j]);
-                min=Math.min(min,nums[j]);
-                sum+=max-min;
-            }
-        }
-        return sum;
+        //     for(int j=i;j<nums.length;j++)
+        //     {
+        //         max=Math.max(max,nums[j]);
+        //         min=Math.min(min,nums[j]);
+        //         sum+=max-min;
+        //     }
+        // }
+        // return sum;
+
+
+        //trying 0(n) solution by my own 
+        // remeber taking in nse or nge use equals aslo in condition for handling edge cases.
         
+        Stack<Integer> st= new Stack<>();
+         int n= arr.length;
+         int pse[]= new int[n];
+         int nse[]= new int [n];
+         int pge[]= new int[n];
+         int nge[]= new int [n];
+         long mod= (long)1e9+7;
+        
+        for(int i=0;i<n;i++) //pse
+        {
+            while(!st.isEmpty() && arr[st.peek()]>arr[i])
+            st.pop();
+
+            pse[i]= st.isEmpty()?-1:st.peek();
+            st.push(i);
+        }
+        st.clear();
+
+        for(int i=n-1;i>=0;i--) //nse
+        {
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+            st.pop();
+
+            nse[i]= st.isEmpty()?n:st.peek();
+            st.push(i);
+        }
+        st.clear();
+         for(int i=0;i<n;i++) //pge
+        {
+            while(!st.isEmpty() && arr[st.peek()]<arr[i])
+            st.pop();
+
+            pge[i]= st.isEmpty()?-1:st.peek();
+            st.push(i);
+        }
+        st.clear();
+
+        for(int i=n-1;i>=0;i--) // nge
+        {
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i])
+            st.pop();
+
+            nge[i]= st.isEmpty()?n:st.peek();
+            st.push(i);
+        }
+        st.clear();
+        long ans=0;
+         long max=0;long min=0;
+        for(int i=0;i<n;i++)
+        {
+            int lsmall=i-pse[i];
+            int lbig=i-pge[i];
+            int rsmall=nse[i]-i;
+            int rbig=nge[i]-i;
+           
+            max+=(long)(lbig*rbig)*arr[i];
+            min+=(long)(lsmall*rsmall)*arr[i];
+            
+            
+        }
+        return max-min;
+        
+
     }
 }
