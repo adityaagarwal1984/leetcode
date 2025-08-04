9b1e02973aac7674ef85a951404c477b38213046
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-08-04 14:23:02 +0530

    Time: 8 ms (45.08%) | Memory: 40.2 MB (80.72%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int fib(int n)
+    {
+        if(n<=1)
+        return n;
+        return fib(n-1)+fib(n-2);
+    }
+    // public int fib(int n) {
+        
+    // int dp[]= new int[n+1];
+    // for(int i=0;i<n+1;i++)
+    // {
+    //     dp[i]=-1;
+    // }
+
+    //  return f(n,dp);
+        
+    // }
+    // public int f(int n,int dp[])
+    // {
+    //     if(n<=1)
+    //     return n;
+    //     if(dp[n]!=-1)
+    //     return dp[n];
+    //     else
+    //     return dp[n]= f(n-1,dp)+f(n-2,dp);
+    // }
+}
