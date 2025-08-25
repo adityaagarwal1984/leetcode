ba5d1406d30a60c428328d201bd8db8a57cf2767
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-08-26 02:40:34 +0530

    Time: 36 ms (76.25%) | Memory: 44.5 MB (49.93%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,28 +1,52 @@
-class Solution {
+// class Solution {
+   
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int dp[][]= new int[n][n+1];
+//         for(int i=0;i<n;i++)
+//         {
+//             Arrays.fill(dp[i],-1);
+//         }
+// return ans(nums,0,-1,dp);
+        
+//     }
+//     public int ans(int arr[],int i,int prev,int dp[][])
+//     {
+//         if(i==arr.length)
+//         return 0;
+//         if(dp[i][prev+1]!=-1)
+//         return dp[i][prev+1];
+//         int nottake=ans(arr,i+1,prev,dp);
+//         int take=0;
+//         if(prev==-1 || arr[i]>arr[prev]){
+//         take= 1+ans(arr,i+1,i,dp);
+        
+//         }
+        
+//         return dp[i][prev+1]= Math.max(take,nottake);
+//     }
+// }
+ class Solution {
    
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int dp[][]= new int[n][n+1];
+        int dp[]= new int[n];
+        Arrays.fill(dp,1);
+        int max=1;
         for(int i=0;i<n;i++)
         {
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    dp[i]= Math.max(dp[i],1+dp[j]);
+                }
+            }
         }
-return ans(nums,0,-1,dp);
-        
-    }
-    public int ans(int arr[],int i,int prev,int dp[][])
-    {
-        if(i==arr.length)
-        return 0;
-        if(dp[i][prev+1]!=-1)
-        return dp[i][prev+1];
-        int nottake=ans(arr,i+1,prev,dp);
-        int take=0;
-        if(prev==-1 || arr[i]>arr[prev]){
-        take= 1+ans(arr,i+1,i,dp);
-        
+        for(int num:dp)
+        {
+            max=Math.max(max,num);
         }
-        
-        return dp[i][prev+1]= Math.max(take,nottake);
+        return max;
     }
-}
+ }
