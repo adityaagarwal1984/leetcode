675226f11d5cd983a0be050df0b4bbdd39266092
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-09-05 01:36:18 +0530

    Time: 0 ms (100%), Space: 44.4 MB (80.51%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -8,28 +8,22 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        // Edge case: if the list is empty or has only one node, return it as is
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode odd = head;           // Points to the first node (odd index)
-        ListNode even = head.next;     // Points to the second node (even index)
-        ListNode evenHead = even;      // Stores the head of even nodes to link later
-
-        while (even != null && even.next != null) {
-            odd.next = even.next;      // Connecting odd nodes
-            odd = odd.next;            // Move to next odd node
-            
-            even.next = odd.next;      // Connecting even nodes
-            even = even.next;          // Move to next even node
-        }
-
-        odd.next = evenHead; // Append even nodes after odd nodes
+        if(head==null || head.next==null)
         return head;
+        ListNode odd=head;
+        ListNode even=odd.next;
+        ListNode evenhead=even;
+        while(even!=null && even.next!=null)
+        {
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
+        }
+        odd.next=evenhead;
+        return head;
+        
     }
 }
-
