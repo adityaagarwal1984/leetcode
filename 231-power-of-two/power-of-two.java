d9f0e32dea04bbed4ffc71b5dabc9fe74ff7675e
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-02 21:56:34 +0530

    Time: 1 ms (31.58%) | Memory: 42.8 MB (5.30%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -2,10 +2,7 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0)
         return false;
-        if((n & (n-1))==0)
-        return true;
-        else
-        return false;
+        return (n&(n-1))==0;
         
     }
 }
