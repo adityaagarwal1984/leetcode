2f9894963bc2e17665739916b0e1aab3fb8af1b6
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-09 21:30:36 +0530

    Time: 27 ms (27.12%) | Memory: 47.7 MB (6.07%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,32 +1,30 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < num.length(); i++) {
-            char ch = num.charAt(i);
-            while (!st.isEmpty() && k > 0 && st.peek() > ch) {
-                st.pop();
-                k--;
-            }
-            st.push(ch);
-        }
-
-        // Remove remaining digits from the end if k > 0
-        while (k-- > 0 && !st.isEmpty()) {
+        Stack<Integer> st= new Stack<>();
+        for(char  ch : num.toCharArray())
+        {
+            int n=ch-'0';
+            while(!st.isEmpty() && k>0 && n<st.peek()){
             st.pop();
-        }
+            k--;
+            }
 
-        // Build the result string
-        StringBuilder sb = new StringBuilder();
-        for (char c : st) {
-            sb.append(c);
+            st.push(n);
         }
-
-        // Remove leading zeros
-        while (sb.length() > 0 && sb.charAt(0) == '0') {
-            sb.deleteCharAt(0);
+        while(k > 0 && !st.isEmpty()) {
+            st.pop();
+            k--;
         }
+        StringBuilder sb= new StringBuilder();
+        while(!st.isEmpty())
+        {
+            sb.append(st.pop());
+        }
+        String s=sb.reverse().toString().replaceFirst("^0+","");
 
-        return sb.length() == 0 ? "0" : sb.toString();
+        if(s.isEmpty())
+        return "0";
+        return s;
+        
     }
 }
