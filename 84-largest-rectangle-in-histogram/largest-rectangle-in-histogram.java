971cc04727f507bfea5d94134c1ceeb976e7e674
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-09 22:24:21 +0530

    Time: 112 ms (20.61%) | Memory: 76.2 MB (9.18%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,42 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        int pse[]= pse(heights);
+        int nse[]= nse(heights);
         int max=Integer.MIN_VALUE;
+        for(int i=0;i<heights.length;i++)
+        {
         
-        Stack<Integer> st= new Stack<>();
-        int n=heights.length;
-        int nse[]=new int[n];
-        int pse[]= new int[n];
-        for(int i= n-1;i>=0;i--)
-        {
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i] )
-            st.pop();
-            nse[i]= st.isEmpty()?n:st.peek();
-            st.push(i);
-        }
-        st.clear();
-        for(int i=0;i<n;i++)
-        {
-            while(!st.isEmpty() && heights[st.peek()]>heights[i] )
-            st.pop();
-            pse[i]= st.isEmpty()?-1:st.peek();
-            st.push(i);
-        }
-        int total=0;
-        for(int i=0;i<n;i++)
-        {
-            
-            int ans= heights[i]*(nse[i]-pse[i]-1);
-            max=Math.max(max,ans);
+            max=Math.max(max,(nse[i]-pse[i]-1)*heights[i]);
         }
         return max;
         
+    }
+    public int[] nse(int arr[])
+    {
+        int ans[]= new int[arr.length];
+        Stack<Integer> st= new Stack<>();
+        int n=arr.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+            st.pop();
+            ans[i]=st.isEmpty()?n:st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] pse(int arr[])
+    {
+        int ans[]= new int[arr.length];
+        Stack<Integer> st= new Stack<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+            st.pop();
+            ans[i]=st.isEmpty()?-1:st.peek();
+            st.push(i);
+        }
+        return ans;
     }
 }
