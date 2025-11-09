472586ea7da5b624abb0c5e2d299cd9ecbe4f763
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-09 18:08:18 +0530

    Time: 72 ms (37.60%) | Memory: 57.6 MB (5.33%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,37 +1,43 @@
+import java.util.*;
+
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        Stack<Integer> st= new Stack<>();
-         int n= arr.length;
-         int left[]= new int[n];
-         int right[]= new int [n];
-         long mod= (long)1e9+7;
-        
-        for(int i=0;i<n;i++)
-        {
-            while(!st.isEmpty() && arr[st.peek()]>arr[i])
-            st.pop();
+        int n = arr.length;
+        int mod = 1000000007;
+        int[] pse = prevSmaller(arr);
+        int[] nse = nextSmaller(arr);
 
-            left[i]= st.isEmpty()?i+1:i-st.peek();
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - pse[i];
+            long right = nse[i] - i;
+            long contrib = (arr[i] * (left * right % mod)) % mod;
+            sum = (sum + contrib) % mod;
+        }
+        return (int) sum;
+    }
+
+    private int[] prevSmaller(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] >arr[i]) st.pop();
+            ans[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
-        st.clear();
+        return ans;
+    }
 
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
-            st.pop();
-
-            right[i]= st.isEmpty()?n-i:st.peek()-i;
+    private int[] nextSmaller(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+            ans[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
-        long ans=0;
-        for(int i=0;i<n;i++)
-        {
-            ans = (ans + (long) left[i] * right[i] * arr[i]) % mod;
-
-        }
-        return (int)ans;
-        
-        
+        return ans;
     }
 }
