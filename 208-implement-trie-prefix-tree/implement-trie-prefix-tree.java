de0ad1d4971282c285ba362655cfbdacc8e1ba14
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-18 13:51:46 +0530

    Time: 35 ms (58.13%) | Memory: 62.4 MB (8.85%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,79 +1,73 @@
-class Node{
-    Node links[]=new Node[26];
-    boolean flag=false;
 
-    public boolean containskey(char ch)
-    {
-        return links[ch-'a']!=null;
-    }
-    public Node get(char ch)
-    {
-        return links[ch-'a'];
-    }
-    public void put(char ch,Node node)
-    {
-        links[ch-'a']=node;
-    }
-    public void setend()
-    {
-        flag = true;
-    }
-    public boolean isend()
-    {
-        return flag;
-    }
+class Node
+{
+Node list[]= new Node[26];
+boolean end;
+public boolean contains(char ch)
+{
+    return list[ch-'a']!=null;
 }
-class Trie {
- private static Node root;
+public Node get(char ch)
+{
+    return list[ch-'a'];
+}
+public void put(char ch,Node node)
+{
+    list[ch-'a']=node;
+}
+public void setEnd()
+{
+    end=true;
+}
+public boolean isEnd()
+{
+    return end;
+}
+}
+    class Trie {
+    private static Node root;
     public Trie() {
-       root= new Node();
-
-        
+        root=new Node();
     }
     
     public void insert(String word) {
         Node node=root;
         for(int i=0;i<word.length();i++)
         {
-            if(!node.containskey(word.charAt(i)))
+            if(!node.contains(word.charAt(i)))
             {
-                node.put(word.charAt(i),new Node());
+                char ch=word.charAt(i);
+                node.put(ch,new Node());
             }
             node=node.get(word.charAt(i));
         }
-        node.setend();
+        node.setEnd();
+        
     }
     
     public boolean search(String word) {
         Node node=root;
         for(int i=0;i<word.length();i++)
         {
-            if(!node.containskey(word.charAt(i)))
-            {
-                // node.put(word.charAt(i),new Node());
-                return false;
-            }
-            node=node.get(word.charAt(i));
-        }
-        if(node.isend())
-        return true;
+        if(!node.contains(word.charAt(i)))
         return false;
-        
+        node=node.get(word.charAt(i));
+        }
+        if(node.isEnd())
+        return true;
+        else
+        return false;
     }
     
     public boolean startsWith(String word) {
         Node node=root;
         for(int i=0;i<word.length();i++)
         {
-            if(!node.containskey(word.charAt(i)))
-            {
-                // node.put(word.charAt(i),new Node());
-                return false;
-            }
+            if(!node.contains(word.charAt(i)))
+            return false;
             node=node.get(word.charAt(i));
         }
         return true;
-        
     }
 }
 
