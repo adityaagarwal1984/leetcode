3a1695e8081eee7e33d5bf9484df68086f5f3628
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-11-27 15:19:51 +0530

    Time: 6 ms (58.45%) | Memory: 47.1 MB (41.92%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,32 +7,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==p || root==q || root==null)
+        if(root==null || root==p || root==q)
         return root;
-        if((root.left==p && root.right==q)||(root.left==q && root.right==p))
-        return root;
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q); 
-        if(left==null)
+        TreeNode left= lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+         if(left==null)
         return right;
         else if(right==null)
         return left;
         else
         return root;
-    //     if(root==null || root==p || root==q)
-    //     return root;
-    //     int cur= root.val;
-    //     if(cur>p.val && cur>q.val)
-    //    return lowestCommonAncestor(root.left,p,q);
-    //     if(cur<p.val && cur<q.val)
-    //    return  lowestCommonAncestor(root.right,p,q);
-
-
-    //     return root;
-
-        
     }
 }
