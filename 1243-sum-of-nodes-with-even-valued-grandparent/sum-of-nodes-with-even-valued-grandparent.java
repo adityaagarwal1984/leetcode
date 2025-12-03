42e9d222088b1d51e40d34575b5ef0a705c1a53a
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2025-12-03 10:12:43 +0530

    Time: 1 ms (100.00%) | Memory: 47.6 MB (37.36%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -15,33 +15,56 @@
  */
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
-        int sum=0;
-        Set<TreeNode> set= new HashSet<>();
-        helper(root,set);
-        for(TreeNode num:set)
-        {
-            if(num !=null)
-            {
-                if(num.left!=null)
-                sum+=num.left.val;
-                if(num.right!=null)
-                sum+=num.right.val;
-            }
-        }
-        return sum;
+        if(root==null)
+        return 0;
+        return dfs(root,null,null);
         
     }
-    public void helper(TreeNode root,Set<TreeNode> set)
+    public int dfs(TreeNode root,TreeNode parent,TreeNode baba)
     {
         if(root==null)
-        return;
-        if(root.val%2==0)
+        return 0;
+        int sum=0;
+        if(baba!=null && baba.val%2==0)
         {
-            set.add(root.left);
-            set.add(root.right);
+            sum+=root.val;
         }
-        helper(root.left,set);
-        helper(root.right,set);
-    }
+        sum+=dfs(root.left,root,parent);
+        sum+=dfs(root.right,root,parent);
+        return sum;
 
+    }
 }
+
+// class Solution {
+//     public int sumEvenGrandparent(TreeNode root) {
+//         int sum=0;
+//         Set<TreeNode> set= new HashSet<>();
+//         helper(root,set);
+//         for(TreeNode num:set)
+//         {
+//             if(num !=null)
+//             {
+//                 if(num.left!=null)
+//                 sum+=num.left.val;
+//                 if(num.right!=null)
+//                 sum+=num.right.val;
+//             }
+//         }
+//         return sum;
+        
+//     }
+//     public void helper(TreeNode root,Set<TreeNode> set)
+//     {
+//         if(root==null)
+//         return;
+//         if(root.val%2==0)
+//         {
+//             set.add(root.left);
+//             set.add(root.right);
+//         }
+//         helper(root.left,set);
+//         helper(root.right,set);
+//     }
+
+// }
