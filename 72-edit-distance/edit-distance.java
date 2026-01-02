28736fc50ace6a8b32727eb6eb368e02feae3535
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-01-02 22:16:46 +0530

    Time: 3 ms (96.46%) | Memory: 47.1 MB (69.57%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,29 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int arr[][]= new int[word1.length()][word2.length()];
+        int dp[][]= new int[word1.length()][word2.length()];
         for(int i=0;i<word1.length();i++)
-        {
-            Arrays.fill(arr[i],-1);
-        }
-        return match(word1,word2,word1.length()-1,word2.length()-1,arr);
+        Arrays.fill(dp[i],Integer.MAX_VALUE);
+        return calculate(word1,word2,word1.length()-1,word2.length()-1,dp);
     }
-    public int match(String s1,String s2,int i,int j,int arr[][])
+    public int calculate(String st1,String st2,int idx1,int idx2,int dp[][])
     {
-        if(j<0)
-        return i+1;
-        if(i<0)
-        return j+1;
-        if(arr[i][j]!=-1)
-        return arr[i][j];
+        if(idx1<0)
+        return idx2+1;
+        if(idx2<0)
+        return idx1+1;
+        if(dp[idx1][idx2]!=Integer.MAX_VALUE)
+        return dp[idx1][idx2];
+        if(st1.charAt(idx1)==st2.charAt(idx2))
+        return dp[idx1][idx2]=calculate(st1,st2,idx1-1,idx2-1,dp);
+        int insert=1+calculate(st1,st2,idx1,idx2-1,dp);
+        int delete=1+calculate(st1,st2,idx1-1,idx2,dp);
+        int update=1+calculate(st1,st2,idx1-1,idx2-1,dp);
+        return dp[idx1][idx2]=Math.min(Math.min(insert,delete),update);
 
-        if(s1.charAt(i)== s2.charAt(j))
-         return arr[i][j]= match(s1,s2,i-1,j-1,arr);
-         else
-         {
-            int insert=match(s1,s2,i,j-1,arr);
-            int delete=match(s1,s2,i-1,j,arr);
-            int replace=match(s1,s2,i-1,j-1,arr);
-            return arr[i][j]= 1+Math.min(insert,Math.min(delete,replace));
-         }
     }
 }
