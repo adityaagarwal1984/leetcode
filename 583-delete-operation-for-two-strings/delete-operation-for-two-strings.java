ff8d65fcfce06ec205c0ba984cc8f7c1eb89e780
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-01-02 23:31:55 +0530

    Time: 7 ms (69.85%) | Memory: 46.3 MB (67.90%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,43 +1,35 @@
+
 class Solution {
     public int minDistance(String word1, String word2) {
-        int l1=word1.length();
-        int l2=word2.length();
-        int m= word1.length();
-        int n=word2.length();
-        int arr[][]= new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            Arrays.fill(arr[i],-1);
+
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // BASE CONDITIONS (copied directly from recursion)
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = i;      // idx2 < 0
+
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = j;      // idx1 < 0
+
+        // FILL TABLE (bottom-up)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert  = 1 + dp[i][j - 1];
+                    int delete  = 1 + dp[i - 1][j];
+                   
+
+                    dp[i][j] = Math.min(insert,delete);
+                }
+            }
         }
-        int l=lcs(word1,word2,l1-1,l2-1,arr);
-        return (l1-l)+(l2-l);
-    
-        
-    }
-    // public int longestPalindromeSubseq(String s) {
-    //     // first figure out the longest comon subsequence
-    //     StringBuilder sb= new StringBuilder();
-    //     sb.append(s).reverse();
-    //     String st= sb.toString();
-    //     int m= s.length();
-    //     int n=st.length();
-    //     int arr[][]= new int[m][n];
-    //     for(int i=0;i<m;i++)
-    //     {
-    //         Arrays.fill(arr[i],-1);
-    //     }
-    //     return lcs(s,st,s.length()-1,st.length()-1,arr);
-    
-        
-    // }
-    public int lcs(String s1,String s2,int i,int j,int arr[][])
-    {
-        if(i<0 || j<0)
-        return 0;
-        if(arr[i][j]!=-1)
-        return arr[i][j];
-        if(s1.charAt(i) ==s2.charAt(j))
-        return arr[i][j]= 1+lcs(s1,s2,i-1,j-1,arr);
-        return  arr[i][j]=Math.max(lcs(s1,s2,i-1,j,arr),lcs(s1,s2,i,j-1,arr));
+
+        return dp[n][m];
     }
 }
