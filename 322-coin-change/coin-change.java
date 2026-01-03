e071cac325e4bfc0f0ee1dc6438ea7a243288a3a
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-01-03 18:11:57 +0530

    Time: 19 ms (39.51%) | Memory: 47.1 MB (25.24%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,36 +1,40 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[][]= new int[amount+1][coins.length];
-        for(int i=0;i<amount+1;i++)
+        int dp[][]= new int[coins.length][amount+1];
+        for(int i=0;i<coins.length;i++)
         Arrays.fill(dp[i],-1);
-        int ans=noofcoins(coins,amount,coins.length-1,dp);
-        if(ans>=(int)1e9)
+       noofcoins(coins,amount,coins.length-1,dp);
+        if(dp[coins.length-1][amount]>=(int)1e9)
         return -1;
         else
-        return ans;
+        return dp[coins.length-1][amount];
     }
-    public int noofcoins(int coins[],int amount,int idx,int dp[][])
+    public void noofcoins(int coins[],int amount,int idx,int dp[][])
     {
-        if(idx==0)
+        for(int i=0;i<=amount;i++)
         {
-            if(amount%coins[idx]==0)
+            if(i%coins[0]==0)
             {
-                return amount/coins[idx];
+                dp[0][i]= i/coins[0];
             }
             else
             {
-                return (int)1e9;
+            dp[0][i]= (int)1e9;
             }
         }
-        if(dp[amount][idx]!=-1)
-        return dp[amount][idx];
-        int nottake=noofcoins(coins,amount,idx-1,dp);
-        int take=(int)1e9;
-        if(amount>=coins[idx])
+        for(int i=1;i<coins.length;i++)
         {
-            take=1+noofcoins(coins,amount-coins[idx],idx,dp);
+            for(int j=0;j<=amount;j++)
+        {
+        int nottake=dp[i-1][j];
+        int take=(int)1e9;
+        if(j>=coins[i])
+        {
+            take=1+dp[i][j-coins[i]];
         }
-        return dp[amount][idx]=Math.min(nottake,take);
+         dp[i][j]=Math.min(nottake,take);
+        }
+        }
 
     }
 }
