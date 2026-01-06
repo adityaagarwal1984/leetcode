e765c7f3cdc53e9d072d31ed73eef8a23032c6d6
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-01-07 03:16:35 +0530

    Time: 8 ms (31.70%) | Memory: 47.5 MB (10.15%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,34 +1,32 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n= matrix.length;
+        int m= matrix.length;
+        int n=matrix[0].length;
+        int arr[][]= new int[m][n];
+        for(int i=0;i<n;i++)
+        arr[0][i]=matrix[0][i];
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int a=Integer.MAX_VALUE;
+                int b=Integer.MAX_VALUE;
+                int c=Integer.MAX_VALUE;
+    
+                
+                    a=arr[i-1][j];
+                    if(j-1 >=0)
+                    b=arr[i-1][j-1];
+                    if(j+1<n)
+                    c=arr[i-1][j+1];
+                
+                int min=Math.min(a,Math.min(b,c));
+                arr[i][j]=matrix[i][j]+min;
+            }
+        }
         int min=Integer.MAX_VALUE;
-        int arr[][]= new int[n][n];
-        for(int i=0;i<n;i++)
-        {
-            Arrays.fill(arr[i],Integer.MIN_VALUE);
-        }
-        for(int i=0;i<n;i++)
-        {
-        int a=path(matrix,0,i,n,arr);
-        min=Math.min(min,a);
-        }
+        for(int num:arr[arr.length-1])
+        min=Math.min(min,num);
         return min;
-        
-    }
-    public int path(int [][]matrix,int i, int j,int n,int arr[][])
-    {
-        if(i==n || j==n || j<0)
-        return Integer.MAX_VALUE;
-        if(i==n-1)
-        return matrix[i][j];
-        if(arr[i][j]!=Integer.MIN_VALUE)
-        return arr[i][j];
-        
-        int a= path(matrix,i+1,j-1,n,arr);
-        int b=path(matrix,i+1,j,n,arr);
-        int c= path(matrix,i+1,j+1,n,arr);
-        int min=Math.min(a,b);
-        int m=Math.min(min,c);
-        return arr[i][j]=matrix[i][j]+m;
     }
 }
