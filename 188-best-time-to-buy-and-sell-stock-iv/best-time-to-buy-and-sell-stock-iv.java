0ad15ec602bb7b7835315a924dcbb2451584249d
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-01-06 19:39:37 +0530

    Time: 8 ms (31.84%) | Memory: 45.3 MB (40.14%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int maxProfit(int k,int[] prices) {
-        int arr[][][]= new int[prices.length][2][k+1];
-        for (int i = 0; i < prices.length; i++) {
-    for (int j = 0; j < 2; j++) {
-        Arrays.fill(arr[i][j], -1);
+    public int maxProfit(int k, int[] prices) 
+ {
+        int dp[][][]= new int[prices.length][2][k+1];
+        for(int i=0;i<prices.length;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
+        return solve(prices,0,1,k,dp);
     }
-}
-
-return check(prices,0,1,k,arr);
-        
-    }
-    public int check(int arr[],int i,int buy,int cap,int a[][][])
+    public int solve(int arr[],int i,int buy,int cap,int dp[][][])
     {
         if(i==arr.length)
         return 0;
         if(cap==0)
         return 0;
-        if(a[i][buy][cap]!=-1)
-        return a[i][buy][cap];
+        if(dp[i][buy][cap]!=-1)
+        return dp[i][buy][cap];
         if(buy==1)
         {
-            return a[i][buy][cap]= Math.max(-arr[i]+check(arr,i+1,0,cap,a),0+check(arr,i+1,1,cap,a));
+            return dp[i][buy][cap]= Math.max((-arr[i]+solve(arr,i+1,0,cap,dp)),solve(arr,i+1,1,cap,dp));
         }
         else
         {
-            return a[i][buy][cap]= Math.max(arr[i]+check(arr,i+1,1,cap-1,a),check(arr,i+1,0,cap,a));
-
+return dp[i][buy][cap]=Math.max((arr[i]+solve(arr,i+1,1,cap-1,dp)),solve(arr,i+1,0,cap,dp));
         }
     }
 }
