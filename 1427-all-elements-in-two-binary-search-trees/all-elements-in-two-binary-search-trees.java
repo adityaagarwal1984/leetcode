1d4fe3d449f337936d7602dd134cd6c585a296ba
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-08-04 03:36:07 +0530

    Time: 17 ms (76.93%) | Memory: 48.3 MB (95.13%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,22 +15,47 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> ls= new ArrayList<>();
-        if(root1==null && root2==null)
-        return ls;
-        inorder(root1,ls);
-        inorder(root2,ls);
-        Collections.sort(ls);
-        return ls;
-        
+        List<Integer> ans1= new ArrayList<>();
+        List<Integer> ans2= new ArrayList<>();
+        solve(root1,ans1);
+        solve(root2,ans2);
+        List<Integer> ans= new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(i<ans1.size() && j<ans2.size())
+        {
+            if(ans1.get(i)<ans2.get(j))
+            {
+                ans.add(ans1.get(i));
+                i++;
+            }
+            else
+            {
+                ans.add(ans2.get(j));
+                j++;
+            }
+        }
+        while(i<ans1.size())
+        {
+            ans.add(ans1.get(i));
+            i++;
+        }
+        while(j<ans2.size())
+        {
+            ans.add(ans2.get(j));
+            j++;
+        }
+        return ans;
     }
-    public void inorder(TreeNode root,List<Integer> ans)
+    public void solve(TreeNode root, List<Integer> ls)
     {
         if(root==null)
-        return;
-
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
+        {
+            return;
+        }
+        solve(root.left,ls);
+        ls.add(root.val);
+        solve(root.right,ls);
     }
+
 }
