89254aac6e6af95b537613d6e7e3cccc796e4827
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-08-05 00:49:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (8.65%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
@@ -14,17 +14,24 @@
  * }
  */
 class Solution {
-   
     public boolean evaluateTree(TreeNode root) {
-        if(root==null)
-        return false;
+        if(root==null) return true;
         if(root.left==null && root.right==null)
         {
-            return root.val==0?false:true;
+            if(root.val==0)
+            return false;
+            else if(root.val==1)
+            return true;
         }
-         boolean left=evaluateTree(root.left);
-         boolean  right=evaluateTree(root.right);
-        return root.val==2?left|right:left&right;
+        boolean left= evaluateTree(root.left);
+        boolean right= evaluateTree(root.right);
+        if(root.val==2)
+        {
+            return left|| right;
+        }
+        else if(root.val==3)
+        return left && right;
+
+        return true;
     }
-    
 }
