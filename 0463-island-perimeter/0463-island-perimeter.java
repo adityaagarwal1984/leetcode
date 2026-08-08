ba358a246b71e121c26f98ea6f3e2e41d67808eb
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-08-08 14:29:46 +0530

    Time: 4 ms (99.81%), Space: 47.1 MB (69.29%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,31 +1,20 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int perimeter=0;
-        int row_size= grid.length;
-        int col_size= grid[0].length;
-        for(int i=0;i<row_size;i++)
-        {
-            for(int j=0;j<col_size;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    return dfs(grid,i,j,row_size, col_size);
-                }
+         int perimeter = 0;
+    int m = grid.length, n = grid[0].length;
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 1) {
+                perimeter += 4;
+                // If top neighbor is land, subtract shared border (2 sides)
+                if (i > 0 && grid[i - 1][j] == 1) perimeter -= 2;
+                // If left neighbor is land, subtract shared border (2 sides)
+                if (j > 0 && grid[i][j - 1] == 1) perimeter -= 2;
             }
         }
-        return perimeter;
     }
-    public int dfs(int grid[][],int i,int j,int m,int n)
-    {
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0)
-        {
-            return 1;
-        } 
-        if(grid[i][j]==2) return 0;
-        int ans=0;
-        grid[i][j]=2;
-        ans+= dfs(grid,i,j+1,m,n)+ dfs(grid,i,j-1,m,n)+ dfs(grid,i+1,j,m,n)+ dfs(grid,i-1,j,m,n);
-        return ans;
+    return perimeter;
 
     }
 }
