cb3f7815b65b72a1ed60550af249178baf4e506f
Aditya Agarwal <166989963+adityaagarwal1984@users.noreply.github.com>
2026-08-13 18:04:10 +0530

    Time: 83 ms (31.60%) | Memory: 125.8 MB (57.64%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -6,40 +6,39 @@ class Solution {
         {
             ls.add(new ArrayList<>());
         }
-        int dis[]= new int [n];
-        Arrays.fill(dis,Integer.MAX_VALUE);
-        dis[headID]=0;
+        //int dis[]= new int [n];
+       // Arrays.fill(dis,Integer.MAX_VALUE);
+       // dis[headID]=0;
         for(int i=0;i<manager.length;i++)
         {
             int m_id= manager[i];
             if(m_id!=-1)
             ls.get(m_id).add(new int[]{i,informTime[m_id]});
         }
-        PriorityQueue<int[]> que= new PriorityQueue<>((a,b)->
-        {
-            return a[1]-b[1];
-        });
-
+        Queue<int[]> que= new LinkedList<>();
+        int max= Integer.MIN_VALUE;
         que.offer(new int[]{headID,0});
         while(!que.isEmpty())
         {
             int curr[]= que.poll();
             int node= curr[0];
             int dist= curr[1];
-            for(int nei[]: ls.get(node))
+            if(ls.get(node).size()==0)
             {
-                if(dis[nei[0]]> dist+nei[1])
+                max= Math.max(max,dist);
+            }
+            else
+            
+            {
+                for(int arr[]: ls.get(node))
                 {
-                    dis[nei[0]]= dist+nei[1];
-                    que.offer(new int[]{nei[0],dist+nei[1]});
+                    int nei= arr[0];
+                    int time= arr[1];
+                    que.offer(new int[]{nei,dist+time});
                 }
             }
         }
-     int max = Integer.MIN_VALUE;
-     for(int i=0;i<n;i++)
-     {
-        max= Math.max(max,dis[i]);
-     }
+     
      //return max;
      if(max== Integer.MAX_VALUE)
      return -1;
